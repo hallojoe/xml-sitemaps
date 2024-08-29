a345f73c17ff3fdd29fa1495a594e6c41a273af3
Casper <casper.korsgaard@outlook.com>
2024-08-29 23:16:49 +0200

    Cleanup.

--- Casko.AspNetCore.XmlSiteMaps/IXmlSiteMapBase.cs
+++ Casko.AspNetCore.XmlSiteMaps/IXmlSiteMapBase.cs
@@ -2,7 +2,7 @@
 
 /// <summary>
 ///     Used to constraint generic type parameter of <see cref="IXmlSiteMap{T}" /> and
-///     <see cref="IMultilingualXmlSiteMap{T}" />
+///     <see cref="IXmlSiteMapCollection{T}" />
 /// </summary>
 public interface IXmlSiteMapBase
 {
